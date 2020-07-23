2ceb8b71e6e3ef0708aa47ceeea72d03bdd52ec8
efth2007 <efth2007@hotmail.com>
2020-07-23 10:46:38 +0200

    Install morgan, bcrypt and jsonwebtoken

--- seeders/20200721142836-users.js
+++ seeders/20200721142836-users.js
@@ -1,21 +1,23 @@
 "use strict";
+const bcrypt = require("bcrypt");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const saltRounds = 10;
     return queryInterface.bulkInsert(
       "users",
       [
         {
           userName: "one",
           email: "1@1.com",
-          password: "1111",
+          password: bcrypt.hashSync("1111", saltRounds),
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           userName: "test",
           email: "t@t.com",
-          password: "tttt",
+          password: bcrypt.hashSync("tttt", saltRounds),
           createdAt: new Date(),
           updatedAt: new Date(),
         },
