56703cb47531bc48dd962229520f0b9a990789ab
efth2007 <efth2007@hotmail.com>
2020-07-23 13:35:58 +0200

    Signup and login endpoints

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+const authRouter = require("./routers/auth");
 const artistRouter = require("./routers/artist");
 const artworkRouter = require("./routers/artwork");
 const userRouter = require("./routers/user");
+
+const cors = require("cors");
+app.use(cors());
 
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
 app.get("/", (req, res) => res.send("Hello!!"));
 
+app.use("/", authRouter);
 app.use("/artists", artistRouter);
 app.use("/artworks", artworkRouter);
 app.use("/users", userRouter);
